eb73e7087c7a8b0354084986a85e059edff2de5c
ammaryasser25-tech <ammaryasser25@gmail.com>
2025-12-11 07:33:17 +0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./travel_mvp.db"
+DATABASE_URL = "sqlite:///./travel.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
